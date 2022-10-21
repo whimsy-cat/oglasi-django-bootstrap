88d289621ed069999f0aa4e4074dda6cb5ef9f6c
fedirlutsenko <tigerion.1993@gmail.com>
2022-10-21 12:37:05 +0200

    Listings

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def listings(request):
+    return render(request, 'listings.html')
--- oglasi_app/urls.py
+++ oglasi_app/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('oglasi/', include('listings.urls')),
     path('', views.home, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
