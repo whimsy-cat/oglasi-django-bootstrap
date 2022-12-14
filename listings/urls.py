9373c0b0c09a67c8f72c1af80a36498120a75396
fedirlutsenko <tigerion.1993@gmail.com>
2022-12-14 19:15:26 +0100

    Update profile sidebar

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from listings import views
 
 
 urlpatterns = [
-    path('/', views.listings, name="listings"),
+    path('', views.listings, name="listings"),
     path('postavi/', views.create, name="listing_create"),
     path('pregled/<slug>/', views.listing, name="listing_profile"),
 ]
