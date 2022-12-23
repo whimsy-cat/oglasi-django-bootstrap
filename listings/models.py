3cdd438a244df4c5ca34e91406fb95484bd6e53c
fedirlutsenko <tigerion.1993@gmail.com>
2022-12-23 17:00:41 +0100

    Update listing models

--- listings/models.py
+++ listings/models.py
@@ -6,23 +6,41 @@ from category.models import Category
 class Listing(models.Model):
     name = models.CharField(max_length=150, null=False)
     description = models.CharField(max_length=255, null=True)
-    slug = models.CharField(max_length=150, null=False, default="")
-    address = models.CharField(max_length=250, null=False)
-    zipcode = models.CharField(max_length=20)
-    municipality = models.CharField(max_length=250, null=False, default=None)
-    city = models.CharField(max_length=150, null=False, default=None)
-    rooms = models.DecimalField(decimal_places=1, max_digits=4, null=False)
-    baths = models.IntegerField(null=False, default=0)
-    half_baths = models.IntegerField(null=False, default=0)
-    size = models.IntegerField(null=False)
-    year_built = models.IntegerField()
-    date_listed = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=25)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, default=1)
     price = models.IntegerField(null=False)
+    slug = models.CharField(max_length=150, null=False, default="")
+    country = models.CharField(max_length=150, null=False, default="Serbia")
+    city = models.CharField(max_length=150, null=False, default=None)
+    municipality = models.CharField(max_length=250, null=False, default=None)
+    area = models.CharField(max_length=250, null=False, default=None)
+    address = models.CharField(max_length=250, null=False)
+    date_listed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class ListingCharacteristics(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    size = models.IntegerField(null=False)
+    structure = models.DecimalField(decimal_places=1, max_digits=4, null=False)
+    year_built = models.IntegerField(null=True)
+    floor = models.IntegerField(null=True)
+    total_floors = models.IntegerField(null=True)
+    bedrooms = models.DecimalField(decimal_places=1, max_digits=4, null=False)
+    baths = models.IntegerField(null=False, default=0)
+    half_baths = models.IntegerField(null=False, default=0)
+    parking = models.CharField(null=False, max_length=50)
+    parking_slots = models.IntegerField(null=False, default=0)
+    balcony = models.CharField(null=False, max_length=50)
+    basement = models.BooleanField(null=False, default=False)
+    storage = models.BooleanField(null=False, default=False)
+    legal_status = models.CharField(null=False, max_length=50)
+    condition = models.CharField(null=False, max_length=50)
+    efficiency = models.CharField(null=False, max_length=50)
+    efficiency_index = models.CharField(null=False, max_length=50)
+
 
 
 class Detail(models.Model):
@@ -54,8 +72,7 @@ class ListingAmenities(models.Model):
 
 class ListingFloorPlan(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, default=None)
-    floor_2d = models.ImageField(upload_to="photos/floor2d")
-    floor_3d = models.ImageField(upload_to="photos/floo3rd")
+    floor_plan = models.CharField(null=False, max_length=255, default=None)
 
 
 class ListingImages(models.Model):
